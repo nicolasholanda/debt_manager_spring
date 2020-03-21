d274a5fa37e79724e6c84ba0b5e2d374038294d8
Nicolas Holanda <nikolas.nhm14@gmail.com>
2020-03-21 15:29:36 -0300

    Criação da entidade Endereço

---
+++ src/main/java/com/github/nicolasholanda/debt/model/Address.java
@@ -0,0 +1,106 @@
+package com.github.nicolasholanda.debt.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address extends BaseEntity<Integer> {
+
+    private String cep;
+    private String city;
+    private String street;
+    private String number;
+    private String country;
+    private String district;
+    private Double latitude;
+    private Double longitude;
+    private String complement;
+
+    public Address() {
+    }
+
+    public Address(String cep, String city, String street, String number, String country, String district, Double latitude, Double longitude, String complement) {
+        this.cep = cep;
+        this.city = city;
+        this.street = street;
+        this.number = number;
+        this.country = country;
+        this.district = district;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.complement = complement;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
